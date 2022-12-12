dddec9abefe013024cb0aef6a0cc0bf1bb8b6d7a
GeniusDP <zaranikbz@gmail.com>
2022-12-12 21:33:26 +0200

    added descriptions to DependencyGraph class

--- src/test/java/org/example/models/DependencyGraphTest.java
+++ src/test/java/org/example/models/DependencyGraphTest.java
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("model testing")
 class DependencyGraphTest {
 
   @Test
